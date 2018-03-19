4da2b1caaf7487ec3c7d3592ffe7c0a645d72321
Titogelo <titogelo@gmail.com>
2018-03-19 16:54:02 +0000

    first step towards popups

--- src/views/classroom/index.js
+++ src/views/classroom/index.js
@@ -19,7 +19,7 @@ class Classroom extends Component {
   constructor() {
     super();
 
-    this.state = { showNewForm: true }
+    this.state = { showPopup: false }
   }
 
   renderClassrooms(){
@@ -136,7 +136,7 @@ class Classroom extends Component {
 
   toggleMenu(){
     this.setState({
-      showNewForm: !this.state.showNewForm
+      showPopup: !this.state.showPopup
     });
   }
 
@@ -157,13 +157,27 @@ class Classroom extends Component {
     return (
       <div key="homeView">
 
-        {/*this.state.showNewForm ?
-          <div>
-          ESCONDE
-          </div> :
-        <div>
-          MUESTRA
-        </div>*/}
+          <div className={"modal inmodal " + (this.state.showPopup ? 'show' : 'hide') } 
+               tabIndex="-1" role="dialog" aria-hidden="true">
+              <div className="modal-dialog">
+                  <div className="modal-content animated flipInY">
+                      <div className="modal-header">
+                          <button type="button" className="close" data-dismiss="modal" onClick={this.toggleMenu.bind(this)}><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
+                          <h4 className="modal-title">Modal title</h4>
+                          <small className="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
+                      </div>
+                      <div className="modal-body">
+                          <p><strong>Lorem Ipsum is simply dummy</strong> text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                              printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                              remaining essentially unchanged.</p>
+                      </div>
+                      <div className="modal-footer">
+                          <button type="button" className="btn btn-white" onClick={this.toggleMenu.bind(this)}>Close</button>
+                          <button type="button" className="btn btn-primary">Save changes</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
 
         <Toolbar title={"Classroom"} button={this.toggleMenu.bind(this)} buttonText={"New classroom"} />
 
