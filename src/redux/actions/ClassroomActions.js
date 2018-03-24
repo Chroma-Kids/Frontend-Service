5156ac475f895e8a58191ac43a887894b9f2c141
Angel Suarez Fernandez <titogelo@gmail.com>
2018-03-24 12:14:26 +0000

    improving removing #1

--- src/redux/actions/ClassroomActions.js
+++ src/redux/actions/ClassroomActions.js
@@ -35,6 +35,23 @@ export function saveClassroom(classroom, uid) {
   return dispatch => database.ref('classrooms/').push({ ...classroom, uid });
 }
 
-export function deleteClassroom(id) {
-  return dispatch => database.ref('classrooms/').child(id).remove();
+/*
+* To delete a classroom we need to:
+* 1) move the teachers of the classroom to break
+* 2) delete the classroom itself
+**/
+export function deleteClassroom(classroomId) {
+  return dispatch => {
+    database.ref(`classrooms/${classroomId}/teachers`).once('value', (snapshot) => {
+      const teachers = Object.keys(snapshot.val() || {});
+      teachers.forEach((teacherId) => {
+        database.ref('classrooms/').child(classroomId).child('teachers').child(teacherId).remove();
+        database.ref('teachers-non-assigned/').child(teacherId).set(true);
+
+        database.ref('teachers/').child(teacherId).child('classrooms').child(classroomId).remove();
+      });
+    }).then(() => {
+      database.ref('classrooms/').child(classroomId).remove();
+    });
+  }
 }
